fd257fe86b4398057b8c1961951606e6dcee371d
ThaiDo <doquangthai12@gmail.com>
2019-08-04 12:02:35 +0700

    Export HomePage function

--- src/main/main.go
+++ src/main/main.go
@@ -2,14 +2,13 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+//HomePage return JSON response
+func HomePage(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "PONG"})
+}
+
 func main() {
 	r := gin.Default()
-	r.GET(
-		"/ping",
-		func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "pong",
-			})
-		})
+	r.GET("/ping", HomePage)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
